f0c23bf2570d40662048640075e3fec2a4942e05
SrutiranjanPraharaj <SrutiranjanPraharaj@DESKTOP-9L3RMP9>
2020-05-12 16:54:16 +0530

    first commit

---
+++ springboot-aws-sqs/src/main/java/com/ranjan/aws/SpringbootAwsSqsApplication.java
@@ -0,0 +1,13 @@
+package com.ranjan.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsSqsApplication.class, args);
+	}
+
+}
